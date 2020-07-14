4135b3f86230d550108635f3e731d75e277a18a4
Lukáš Stuchlík <stuchlik1@seznam.cz>
2020-07-14 14:40:06 +0200

    Updated client packages, hopefully nothing broken?

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,8 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator';
+import Vue from 'vue';
+import Component from 'nuxt-class-component';
 import Logo from '~/components/Logo.vue';
 
 @Component({
