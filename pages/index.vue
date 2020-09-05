2c603b54efc81ae39923611be2a6c389d7bf53d2
Lukáš Stuchlík <stuchlik1@seznam.cz>
2020-09-05 10:18:39 +0200

    United class typing, progress on navbar

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,10 @@
 <template lang="pug">
     div
         Header
-        div.px-4
-            div.max-w-3xl.bg-white.rounded-lg.mx-auto.my-16.p-16
-                h1.text-2xl.font-medium.mb-2 Let's build some shit
-                h2.font-medium.text-sm.text-indigo-400.mb-4.uppercase.tracking-wide Navbar
+        div(class="px-4")
+            div(class="max-w-3xl bg-white rounded-lg mx-auto my-16 p-16")
+                h1(class="text-2xl font-medium mb-2") Let's build some shit
+                h2(class="font-medium text-sm text-indigo-400 mb-4 uppercase tracking-wide") Navbar
                 | Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis temporibus facilis eveniet asperiores dignissimos porro harum. Nemo sunt quae exercitationem culpa ipsum laudantium expedita at dignissimos sint natus? Quas, odit?
 </template>
 
