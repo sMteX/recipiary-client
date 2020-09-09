f165241961141d7a4284c94e09a7796af3af4ee7
Lukáš Stuchlík <stuchlik1@seznam.cz>
2020-09-09 18:09:53 +0200

    Ingredient selecting now actually works, same with the toggle

--- components/MainSearchBar.vue
+++ components/MainSearchBar.vue
@@ -11,7 +11,7 @@
                     button(class="toggle-part" :class="{ 'active-toggle': !recipesActive }" @click="selectIngredients")
                         | Ingredience
             // recipe results
-            div#recipe-results(class="w-full h-auto bg-white absolute rounded-md mt-1 shadow" v-if="recipesActive")
+            div#recipe-results(class="w-full h-auto bg-white absolute rounded-md mt-1 shadow" v-if="!ingredientsActive")
                 // actually link to existing recipes
                 div(class="result-item") Result 1
                 div(class="result-item") Result 2
@@ -26,27 +26,18 @@
                     | {{ ingredient.name }}
                     font-awesome-icon(icon="check" class="mr-2 my-auto text-orange-600" v-if="ingredient.selected")
             // selected ingredients in input
-            div#selected-ingredients(v-if="!recipesActive" class="w-full absolute ml-2")
-                div(class="selected-ingredient")
-                    | Short
-                    font-awesome-icon(icon="times" size="sm" class="my-auto ml-2 cursor-pointer")
-                div(class="selected-ingredient")
-                    | Slightly longer
-                    font-awesome-icon(icon="times" size="sm" class="my-auto ml-2 cursor-pointer")
-                div(class="selected-ingredient")
-                    | Extra long long long
-                    font-awesome-icon(icon="times" size="sm" class="my-auto ml-2 cursor-pointer")
-                div(class="selected-ingredient")
-                    | Short
-                    font-awesome-icon(icon="times" size="sm" class="my-auto ml-2 cursor-pointer")
+            div#selected-ingredients(v-if="ingredientsActive" class="w-full absolute ml-2")
+                div(v-for="ingredient in selectedIngredients" :key="ingredient.id" class="selected-ingredient")
+                    | {{ ingredient.name }}
+                    font-awesome-icon(icon="times" size="sm" class="my-auto ml-2 cursor-pointer" @click="toggleIngredient(ingredient.id)")
         div(class="relative inline-block h-full w-1/5")
             button#search-button(class="h-full w-full bg-orange-500 hover:bg-orange-600 py-2 px-4 text-white font-bold tracking-wide rounded-lg rounded-l-none text-lg")
                 font-awesome-icon(icon="search" class="mr-4")
                 | Hledat
             div#new-toggle(class="absolute right-0 top-0 flex justify-between")
                 div(class="relative w-10 mr-2")
-                    input(type="checkbox" name="toggle" id="toggle-checkbox" class="transition duration-200 ease-in mx-1 shadow absolute w-4 h-4 rounded-full bg-white appearance-none cursor-pointer")
-                    label#toggle-label(for="toggle-checkbox" class=" block h-6 rounded-full bg-gray-400 cursor-pointer")
+                    input(type="checkbox" name="toggle" id="toggle-checkbox" v-model="ingredientsActive" class="transition duration-200 ease-in mx-1 shadow absolute w-4 h-4 rounded-full bg-white appearance-none cursor-pointer")
+                    label#toggle-label(for="toggle-checkbox" class=" block h-6 rounded-full bg-gray-300 cursor-pointer")
                 label(for="toggle-checkbox" class="text-white my-auto") Ingredience
 
 </template>
@@ -56,7 +47,7 @@ export default {
     name: "MainSearchBar",
     data() {
         return {
-            recipesActive: false,
+            ingredientsActive: false,
             // TODO: this should be probably live-retrieved from server if possible
             recipes: [
                 'Kuřecí řízek',
@@ -71,35 +62,32 @@ export default {
                 'Špagety'
             ],
             ingredients: [
-                { id: 1, selected: false, name: 'sůl' },
-                { id: 2, selected: false, name: 'pepř' },
-                { id: 3, selected: false, name: 'špagety' },
-                { id: 4, selected: false, name: 'kuřecí maso' },
-                { id: 5, selected: false, name: 'paprika' },
-                { id: 6, selected: false, name: 'rajčata' },
-                { id: 7, selected: false, name: 'brambory' },
-                { id: 8, selected: false, name: 'strouhanka' }
+                { id: 1, selected: true, name: 'sůl' },
+                { id: 2, selected: true, name: 'pepř' },
+                { id: 3, selected: true, name: 'špagety' },
+                { id: 4, selected: true, name: 'kuřecí maso' },
+                { id: 5, selected: true, name: 'paprika' },
+                { id: 6, selected: true, name: 'rajčata' },
+                { id: 7, selected: true, name: 'brambory' },
+                { id: 8, selected: true, name: 'strouhanka' }
             ]
         }
     },
     computed: {
+        selectedIngredients() {
+            return this.ingredients.filter(i => i.selected);
+        },
         placeholder() {
-            if (this.recipesActive) {
-                return "Vyhledat recepty";
+            if (this.ingredientsActive) {
+                if(this.selectedIngredients.length > 0) {
+                    return "";
+                }
+                return "Vyhledat podle ingrediencí";
             }
-            if(this.ingredients.filter(i => i.selected).length > 0) {
-                return "";
-            }
-            return "Vyhledat podle ingrediencí";
+            return "Vyhledat recepty";
         }
     },
     methods: {
-        selectRecipes() {
-            this.recipesActive = true;
-        },
-        selectIngredients() {
-            this.recipesActive = false;
-        },
         toggleIngredient(id) {
             const ingredient = this.ingredients.find(i => i.id === id);
             ingredient.selected = !ingredient.selected;
@@ -117,7 +105,7 @@ export default {
         transform: translate(0, -50%);
     }
     #toggle-checkbox:checked {
-        @apply bg-orange-500;
+        @apply bg-orange-400;
         transform: translate(100%, -50%);
     }
     //#toggle-checkbox:checked + #toggle-label {
