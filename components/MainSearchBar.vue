5364a1df1ab4add10aac3828ca078209c2499583
Lukáš Stuchlík <stuchlik1@seznam.cz>
2020-09-13 10:42:26 +0200

    Implemented focusing on the search bar - focusing it now shows the result window and correctly hides it

--- components/MainSearchBar.vue
+++ components/MainSearchBar.vue
@@ -1,26 +1,29 @@
 <template lang="pug">
     div(class="h-full")
         // inline block so it creates a new relative-absolute context for input-related things
-        div(class="relative inline-block h-full w-4/5")
-            input(type="text" :placeholder="placeholder" class="h-full w-full rounded-lg rounded-r-none text-lg px-3")
+        div(class="relative inline-block h-full w-4/5"
+            @focusout="inputBlur"
+            @focusin="inputFocus")
+            input(ref="input" type="text" :placeholder="placeholder" :readonly="ingredientsActive"
+                class="h-full w-full rounded-lg rounded-r-none text-lg px-3")
             // recipe results
-            div#recipe-results(class="w-full h-auto bg-white absolute rounded-md mt-1 shadow" v-if="!ingredientsActive")
+            div#recipe-results(class="w-full h-auto bg-white absolute rounded-md mt-1 shadow" v-if="!ingredientsActive" v-show="showResult" @click="$refs.input.focus()")
                 // actually link to existing recipes
-                div(class="result-item") Result 1
-                div(class="result-item") Result 2
-                div(class="result-item") Result 3
+                div(class="result-item refocus-input") Result 1
+                div(class="result-item refocus-input") Result 2
+                div(class="result-item refocus-input") Result 3
             // ingredient results
-            div#ingredient-results(class="w-full h-auto bg-white absolute rounded-md mt-1 shadow" v-else)
+            div#ingredient-results(class="w-full h-auto bg-white absolute rounded-md mt-1 shadow" v-else v-show="showResult")
                 div(v-for="ingredient in ingredients"
                     :key="ingredient.id"
-                    class="result-item"
+                    class="result-item refocus-input"
                     :class="{ 'bg-orange-200': ingredient.selected }"
                     @click="toggleIngredient(ingredient.id)")
                     | {{ ingredient.name }}
                     font-awesome-icon(icon="check" class="mr-2 my-auto text-orange-600" v-if="ingredient.selected")
             // selected ingredients in input - overflows when too much of them are present but CBA to think of a fix
             div#selected-ingredients(v-if="ingredientsActive" class="w-full absolute ml-2")
-                div(v-for="ingredient in selectedIngredients" :key="ingredient.id" class="selected-ingredient")
+                div(v-for="ingredient in selectedIngredients" :key="ingredient.id" class="selected-ingredient refocus-input")
                     | {{ ingredient.name }}
                     font-awesome-icon(icon="times" size="sm" class="my-auto ml-2 cursor-pointer" @click="toggleIngredient(ingredient.id)")
         div(class="relative inline-block h-full w-1/5")
@@ -42,6 +45,7 @@ export default {
     name: "MainSearchBar",
     data() {
         return {
+            showResult: false,
             ingredientsActive: false,
             // TODO: this should be probably live-retrieved from server if possible
             recipes: [
@@ -83,9 +87,38 @@ export default {
         }
     },
     methods: {
+        inputBlur(event) {
+            // for both recipe results and ingredient result (click inside div)
+            const hasResultItemClass =
+                event.explicitOriginalTarget &&
+                event.explicitOriginalTarget.classList &&
+                event.explicitOriginalTarget.classList.contains('refocus-input') || false;
+            // for clicking directly on the text in the result (explicit result is text node with refocus-input SOMEWHERE as a parent)
+            let hasResultItemParent = false;
+            let current = event.explicitOriginalTarget;
+            // normally checking for parent.refocus-input would be enough but it breaks on the SVG check-mark
+            while (current.parentElement) {
+                if (current.parentElement.classList &&
+                    current.parentElement.classList.contains('refocus-input')) {
+                    hasResultItemParent = true;
+                    break;
+                }
+                current = current.parentElement;
+            }
+            if (hasResultItemClass || hasResultItemParent) {
+                return;
+            }
+            // only clicks that are outside the input or the result window should cause the results to go away
+            this.showResult = false;
+        },
+        inputFocus() {
+            // TODO: probably should show after you type something, but the mechanism for showing/hiding is the same
+            this.showResult = true;
+        },
         toggleIngredient(id) {
             const ingredient = this.ingredients.find(i => i.id === id);
             ingredient.selected = !ingredient.selected;
+            this.$refs.input.focus();
         }
     }
 }
@@ -104,23 +137,8 @@ export default {
         transform: translate(100%, -50%);
     }
 }
-
 #search-button {
     min-width: 130px;
-}
-.active-toggle {
-    @apply bg-white text-orange-500 rounded-full;
-}
-.toggle-part {
-    @apply inline-flex items-center px-4 py-2;
-
-    &:focus {
-        @apply outline-none text-orange-500;
-    }
-
-    &:hover {
-        @apply text-orange-500;
-    }
 }
 .result-item {
     @apply cursor-pointer py-1 px-2 flex justify-between;
@@ -141,7 +159,7 @@ export default {
     @apply overflow-hidden;
 }
 .selected-ingredient {
-    @apply mr-1 px-2 py-1 rounded-md border border-orange-500 text-orange-600 flex justify-between inline-block;
+    @apply mr-1 px-2 py-1 rounded-md border border-orange-500 text-orange-600 flex justify-between inline-block cursor-default;
 
     &:hover {
         @apply border-orange-700 text-orange-700;
