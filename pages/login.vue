0f94063a26cddaa5b628c6332af15e5bb4c4bef8
Lukáš Stuchlík <stuchlik1@seznam.cz>
2020-09-14 21:54:13 +0200

    Login page

--- pages/login.vue
+++ pages/login.vue
@@ -1,53 +1,56 @@
-<template>
-    <div>
-        <p>Username:</p>
-        <input type="text" v-model="username" />
-        <p>Password:</p>
-        <input type="password" v-model="password" />
-        <button type="submit" @click="login">Login</button>
-        <p v-if="loggedIn">Logged in</p>
-        <button v-if="loggedIn" @click="logout">Logout</button>
-        <p v-if="error">Error: {{ error }}</p>
-    </div>
+<template lang="pug">
+    div(class="w-1/3 m-auto h-auto bg-black bg-opacity-50 p-12 rounded-md mt-20")
+        h1(class="text-white font-sans text-4xl font-medium tracking-wide antialiased") Přihlásit se
+        div(class="relative rounded-md mt-4")
+            div(class="absolute top-0 left-0 rounded-md rounded-r-none bg-gray-100 p-2")
+                font-awesome-icon(icon="user" class="login-icon")
+            input(type="text" v-model="username" placeholder="Uživatelské jméno" class="w-full rounded-md p-2 pl-10 bg-gray-100 shadow-inner")
+        div(class="relative rounded-md mt-4")
+            div(class="absolute top-0 left-0 rounded-md rounded-r-none bg-gray-100 p-2")
+                font-awesome-icon(icon="key" class="login-icon")
+            input(type="password" v-model="password" placeholder="Heslo" class="w-full rounded-md p-2 pl-10 bg-gray-100 shadow-inner")
+
+        div(class="w-full flex justify-center mt-6")
+            button(type="submit" class="bg-orange-500 hover:bg-orange-600 px-6 py-2 rounded-md tracking-wide" @click="login") Odeslat
+        div(v-if="error" class="mt-4 text-red-500") Error: {{ error }}
 </template>
 
-<script lang="ts">
-import Vue from 'vue';
-import Component from 'nuxt-class-component';
-
-@Component({
-    head: {
-        title: 'Login'
-    }
-})
-export default class LoginPage extends Vue {
-    username: string = '';
-    password: string = '';
-    error: string = '';
-
-    get loggedIn(): boolean {
-        return this.$auth.loggedIn;
-    }
-
-    async logout() {
-        await this.$auth.logout();
-    }
-
-    async login() {
-        try {
-            await this.$auth.loginWith('local', {
-                data: {
-                    username: this.username,
-                    password: this.password
-                }
-            })
-        } catch (e) {
-            this.error = e.response.data.message;
+<script>
+export default {
+    name: 'LoginPage',
+    data() {
+        return {
+            username: '',
+            password: '',
+            error: ''
+        };
+    },
+    head() {
+        return {
+            title: 'Login'
+        };
+    },
+    methods: {
+        async login() {
+            try {
+                await this.$auth.loginWith('local', {
+                    data: {
+                        username: this.username,
+                        password: this.password
+                    }
+                })
+            } catch (e) {
+                this.error = e.response.data.message;
+            }
         }
     }
 }
 </script>
 
 <style scoped>
-
+.login-icon {
+    @apply text-gray-600;
+    width: 1.25rem;
+    height: 1.25rem;
+}
 </style>
--- plugins/fontawesome.ts
+++ plugins/fontawesome.ts
@@ -1,10 +1,10 @@
 import Vue from 'vue';
 import { library, config } from '@fortawesome/fontawesome-svg-core';
-import { faSearch, faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faCheck, faTimes, faKey, faUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 config.autoAddCss = false;
 
-library.add(faSearch, faCheck, faTimes);
+library.add(faSearch, faCheck, faTimes, faKey, faUser);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
