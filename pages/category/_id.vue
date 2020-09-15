8de6eff7098fc541297efb47690d9f3c2c382232
Lukáš Stuchlík <stuchlik1@seznam.cz>
2020-09-15 10:43:44 +0200

    Scaffold category and recipe pages

---
+++ pages/category/_id.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+    div
+        | Category, id: {{ $route.params.id }}
+</template>
+
+<script>
+export default {
+    name: 'CategoryPage',
+    validate({ params }) {
+        // TODO: category must exist
+        // must be a number
+        return /^\d+$/.test(params.id);
+    }
+}
+</script>
+
+<style>
+
+</style>
