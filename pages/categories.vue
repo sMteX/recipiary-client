7d5b66aa22de4079383584319e8a8bebd3e86179
Lukáš Stuchlík <stuchlik1@seznam.cz>
2020-09-16 16:53:36 +0200

    [WIP] Category pages

--- pages/categories.vue
+++ pages/categories.vue
@@ -1,10 +1,25 @@
 <template lang="pug">
     div
+        div(class="w-auto inline-block px-4 pb-2 border-b-2 border-orange-500")
+            h1(class="font-sans text-white font-bold text-4xl tracking-wide") Kategorie
+        div(class="grid grid-cols-5 gap-8 py-6 px-2")
+            // top level categories
+            CategoryItem(file="Chicken-Tikka-Masala-Square.jpg" title="Oběd")
+            CategoryItem(file="Chicken-Tikka-Masala-Square.jpg" title="Oběd")
+            CategoryItem(file="Chicken-Tikka-Masala-Square.jpg" title="Oběd")
+            CategoryItem(file="Chicken-Tikka-Masala-Square.jpg" title="Oběd")
+            CategoryItem(file="Chicken-Tikka-Masala-Square.jpg" title="Oběd")
+            CategoryItem(file="Chicken-Tikka-Masala-Square.jpg" title="Oběd")
 </template>
 
 <script>
 export default {
-    name: "CategoriesPage"
+    name: "CategoriesPage",
+    head() {
+        return {
+            title: 'Kategorie'
+        }
+    }
 }
 </script>
 
--- pages/category/_id.vue
+++ pages/category/_id.vue
@@ -1,6 +1,15 @@
 <template lang="pug">
     div
-        | Category, id: {{ $route.params.id }}
+        div(class="w-auto inline-block px-4 pb-2 border-b-2 border-orange-500")
+            h1(class="font-sans text-white font-bold text-4xl tracking-wide") Kategorie
+        div(class="grid grid-cols-5 gap-8 py-6 px-2")
+            // top level categories
+            CategoryItem(file="Chicken-Tikka-Masala-Square.jpg" title="Oběd")
+            CategoryItem(file="Chicken-Tikka-Masala-Square.jpg" title="Oběd")
+            CategoryItem(file="Chicken-Tikka-Masala-Square.jpg" title="Oběd")
+            CategoryItem(file="Chicken-Tikka-Masala-Square.jpg" title="Oběd")
+            CategoryItem(file="Chicken-Tikka-Masala-Square.jpg" title="Oběd")
+            CategoryItem(file="Chicken-Tikka-Masala-Square.jpg" title="Oběd")
 </template>
 
 <script>
