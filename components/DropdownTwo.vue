e23eeb4d66de68ba5a7f887b5d4bd2b39d09e952
Lukáš Stuchlík <stuchlik1@seznam.cz>
2020-09-22 01:21:01 +0200

    Expandable category link in header - prototype

---
+++ components/DropdownTwo.vue
@@ -0,0 +1,48 @@
+<template lang="pug">
+    div(class="dropdown relative py-3 px-0 lg:p-4 cursor-pointer")
+        span Kategorie
+        ul(class="dropdown-content dropdown-first absolute hidden bg-black bg-opacity-75 text-white w-56")
+            li
+                a(href="#" class="block") Slightly longer category
+            li
+                a(href="#" class="block") Cat 2
+            li(class="dropdown relative")
+                a(href="#" class="flex justify-between")
+                    | Cat 3
+                    font-awesome-icon(icon="chevron-right")
+                ul(class="dropdown-content absolute hidden bg-black bg-opacity-75 w-56")
+                    li
+                        a(href="#" class="block") Cat 3-1
+                    li
+                        a(href="#" class="block") Cat 3-2
+
+
+</template>
+
+<script>
+export default {
+name: "DropdownTwo"
+}
+</script>
+
+<style scoped lang="scss">
+.dropdown:hover > .dropdown-content {
+    display: block;
+}
+.dropdown-content {
+    left: 100%;
+    top: 0;
+
+    &.dropdown-first {
+        left: 0;
+        top: 100%;
+    }
+}
+a {
+    @apply p-2;
+
+    &:hover {
+        @apply text-orange-500;
+    }
+}
+</style>
--- plugins/fontawesome.ts
+++ plugins/fontawesome.ts
@@ -1,10 +1,10 @@
 import Vue from 'vue';
 import { library, config } from '@fortawesome/fontawesome-svg-core';
-import { faSearch, faCheck, faTimes, faKey, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faCheck, faTimes, faKey, faUser, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 config.autoAddCss = false;
 
-library.add(faSearch, faCheck, faTimes, faKey, faUser);
+library.add(faSearch, faCheck, faTimes, faKey, faUser, faChevronRight);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
