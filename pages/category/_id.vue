98ae8d6d4a6c2f368514646f0d1f59efacc5425e
Lukáš Stuchlík <stuchlik1@seznam.cz>
2020-09-21 22:00:05 +0200

    Category page - title, binding for subcategories

--- pages/category/_id.vue
+++ pages/category/_id.vue
@@ -1,15 +1,10 @@
 <template lang="pug">
     div
         div(class="w-auto inline-block px-4 pb-2 border-b-2 border-orange-500")
-            h1(class="font-sans text-white font-bold text-4xl tracking-wide") Kategorie
+            h1(class="font-sans text-white font-bold text-4xl tracking-wide") {{ name }}
         div(class="grid grid-cols-5 gap-8 py-6 px-2")
-            // top level categories
-            CategoryItem(file="Chicken-Tikka-Masala-Square.jpg" title="Oběd")
-            CategoryItem(file="Chicken-Tikka-Masala-Square.jpg" title="Oběd")
-            CategoryItem(file="Chicken-Tikka-Masala-Square.jpg" title="Oběd")
-            CategoryItem(file="Chicken-Tikka-Masala-Square.jpg" title="Oběd")
-            CategoryItem(file="Chicken-Tikka-Masala-Square.jpg" title="Oběd")
-            CategoryItem(file="Chicken-Tikka-Masala-Square.jpg" title="Oběd")
+            // sub-categories
+            CategoryItem(v-for="category in children" :key="category.id" v-bind="category")
 </template>
 
 <script>
@@ -19,6 +14,22 @@ export default {
         // TODO: category must exist
         // must be a number
         return /^\d+$/.test(params.id);
+    },
+    head() {
+        return {
+            title: this.name
+        }
+    },
+    data() {
+        return {
+            // TODO: fetch from server
+            name: 'Maso',
+            children: [
+                { id: 4, name: 'Kuřecí maso', image: 'Chicken-Tikka-Masala-Square.jpg' },
+                { id: 5, name: 'Vepřové maso', image: 'Chicken-Tikka-Masala-Square.jpg' },
+                { id: 6, name: 'Hovězí maso', image: 'Chicken-Tikka-Masala-Square.jpg' },
+            ]
+        }
     }
 }
 </script>
